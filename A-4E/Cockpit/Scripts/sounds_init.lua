9b904ff70841c397ec5e60d132f7da4bf6e69a66
LevelPulse <alex@reddithon.com>
2016-02-05 22:56:31 +0000

    testtest

---
+++ A-4E/Cockpit/Scripts/sounds_init.lua
@@ -0,0 +1,19 @@
+dofile(LockOn_Options.script_path.."sounds.lua")
+
+sounds = {}
+sounds[SOUND_SW1]		= {"Aircrafts/F-86/Cockpit/Sw1"}
+sounds[SOUND_SW2_ON]	= {"Aircrafts/F-86/Cockpit/Sw2_on"}
+sounds[SOUND_SW2_OFF]	= {"Aircrafts/F-86/Cockpit/Sw2_off"}
+sounds[SOUND_SW3_OPEN]	= {"Aircrafts/F-86/Cockpit/Sw3_Open"}
+sounds[SOUND_SW3_CLOSE]	= {"Aircrafts/F-86/Cockpit/Sw3_Close"}
+sounds[SOUND_SW4_UP]	= {"Aircrafts/F-86/Cockpit/Sw4_Up"}
+sounds[SOUND_SW4_DOWN]	= {"Aircrafts/F-86/Cockpit/Sw4_Down"}
+sounds[SOUND_SW5_ON]	= {"Aircrafts/F-86/Cockpit/Sw5_on"}
+sounds[SOUND_SW5_OFF]	= {"Aircrafts/F-86/Cockpit/Sw5_off"}
+sounds[SOUND_SW6]		= {"Aircrafts/F-86/Cockpit/Sw6"}
+sounds[SOUND_SW7_UP]	= {"Aircrafts/F-86/Cockpit/Sw7_Up"}
+sounds[SOUND_SW7_DOWN]	= {"Aircrafts/F-86/Cockpit/Sw7_Down"}
+sounds[SOUND_SW8]		= {"Aircrafts/F-86/Cockpit/Sw8"}
+sounds[SOUND_SW9]		= {"Aircrafts/F-86/Cockpit/Sw9"}
+sounds[SOUND_SW10_ON]	= {"Aircrafts/F-86/Cockpit/Sw10_on"}
+sounds[SOUND_SW10_OFF]	= {"Aircrafts/F-86/Cockpit/Sw10_off"}
