05bcc7b2afd730221b41a9bd168010a342f67927
LevelPulse <alex@reddithon.com>
2016-02-05 23:19:17 +0000

    Missions edited.

---
+++ A-4E/Mods/aircraft/A-4E/Missions/EN/QuickStart/quickstart.lua
@@ -0,0 +1,14 @@
+planes = {
+    { 
+    name = _('Flight over Tbilisi'),
+    file = 'TF-51_flight over town.miz',
+    },
+	{ 
+    name = _('Hot start from Batumi parking'),
+    file = 'TF-51_Hot start.miz',
+    },
+	{ 
+    name = _('Cold start in Krymsk'),
+    file = 'TF-51_Cold Start.miz',
+    },
+}
