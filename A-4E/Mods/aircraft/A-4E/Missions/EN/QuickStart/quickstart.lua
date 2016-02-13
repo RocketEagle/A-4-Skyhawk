3ade865d7f83ad2ff2a953d2e0ffdd5ae2ef9df6
Alex Wiese <alex@reddithon.com>
2016-02-13 00:16:04 +0000

    misiosn

--- A-4E/Mods/aircraft/A-4E/Missions/EN/QuickStart/quickstart.lua
+++ A-4E/Mods/aircraft/A-4E/Missions/EN/QuickStart/quickstart.lua
@@ -1,23 +1,26 @@
 planes = {
     { 
     name = _('Cold Start'),
-    file = 'A-4E_.miz',
-    },
-	{ 
-    name = _('Hot-Takeoff'),
-    file = 'A-4E_takeoff.miz',
-    },
+    file = 'a4e_cold_start.miz',
+	},
 	{ 
     name = _('Free Flight'),
-    file = 'A-4E_free_flight.miz',
+    file = 'a4e_free_flight.miz',
     },
-	name = _('Carrier Landing'),
-    file = 'A-4E_carrier_landing.miz',
+    { 
+    name = _('Ground Attack'),
+    file = 'a4e_ground_attack.miz',
     },
-	name = _('Dogfight'),
-    file = 'A-4E_Dogfight.miz',
+    { 
+    name = _('Dogfight'),
+    file = 'a4e_dogfight.miz',
     },
-	name = _('Ground Attack'),
-    file = 'A-4E_ground_attack.miz',
+	{ 
+    name = _('Takeoff'),
+    file = 'a4e_takeoff.miz',
+    },
+	{ 
+    name = _('Landing'),
+    file = 'a4e_landing.miz',
     },
 }
