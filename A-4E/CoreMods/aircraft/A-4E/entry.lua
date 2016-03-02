47e4480aa0773bcf0f9af2cb1d6265a10ae43df7
Gospadin <gospadin@gmail.com>
2016-03-01 23:52:47 -0700

    fixed finding liveries in the a-4e.zip file

--- A-4E/CoreMods/aircraft/A-4E/entry.lua
+++ A-4E/CoreMods/aircraft/A-4E/entry.lua
@@ -12,6 +12,10 @@ info		 = _("A-4E Skyhawk AI"),
 encyclopedia_path = current_mod_path..'/Encyclopedia',
 })
 
+mount_vfs_liveries_path (current_mod_path.."/Liveries")
+mount_vfs_texture_path  (current_mod_path.."/Textures")
+mount_vfs_texture_path  (current_mod_path.."/Textures/A-4E")
+
 dofile(current_mod_path..'/A-4E.lua')
 dofile(current_mod_path..'/Weapons/A4E_Weapons.lua')
 
