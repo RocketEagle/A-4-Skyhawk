aded10d28bcff0a1e04d25c849bbd5fba20b1c69
gyrovague <nic.gyrovague@gmail.com>
2016-03-03 15:48:24 +0200

    Fix infinite canopy jettison bug
    Seems canopy gets jettisoned at 0.32, prevent state changes and draw
    updates after that.
    
    Canopy texture when jettisoned is camo minecraft for some reason, maybe
    indicates missing texture.

--- A-4E/Mods/aircraft/A-4E/Cockpit/Scripts/Systems/canopy.lua
+++ A-4E/Mods/aircraft/A-4E/Cockpit/Scripts/Systems/canopy.lua
@@ -10,7 +10,7 @@ local Canopy = 71 -- This is the number of the command from command_defs
 
 
 --Creating local variables
-local CANOPY_COMMAND	=	0				
+local CANOPY_COMMAND	=	0   -- 0 closing, 1 opening, 2 jettisoned
 local CANOPY_STATE	=	0		
 
 
@@ -25,11 +25,9 @@ dev:listen_command(Canopy)
 function SetCommand(command,value)			
 	
 	if (command == Canopy) then
-		if (CANOPY_COMMAND == 1) then
-			CANOPY_COMMAND = 0
-		else
-			CANOPY_COMMAND = 1
-		end
+        if CANOPY_COMMAND <= 1 then -- only toggle while not jettisoned
+            CANOPY_COMMAND = 1-CANOPY_COMMAND --toggle
+        end
 	end
 	
 	
@@ -40,15 +38,18 @@ function update()
 	if (CANOPY_COMMAND == 0 and CANOPY_STATE > 0) then
 		-- lower airbrake in increments of 0.02
 		CANOPY_STATE = CANOPY_STATE - 0.01
+        set_aircraft_draw_argument_value(38,CANOPY_STATE)
 	else
-		if (CANOPY_COMMAND == 1 and CANOPY_STATE < 0.90) then --CANOPY_STATE == 0.90 is fully open / CANOPY_STATE == 1 is canopy jettisoned
+		if (CANOPY_COMMAND == 1 and CANOPY_STATE <= 0.31) then
 			-- raise airbrake in increment of 0.02
 			CANOPY_STATE = CANOPY_STATE + 0.01
+            if (CANOPY_STATE > 0.31) then
+                CANOPY_COMMAND = 2  -- prevent further changes, the canopy is gone
+            end
+            set_aircraft_draw_argument_value(38,CANOPY_STATE)
 		end
 	end
 	
-	
-	set_aircraft_draw_argument_value(38,CANOPY_STATE)
 	
 end
 
