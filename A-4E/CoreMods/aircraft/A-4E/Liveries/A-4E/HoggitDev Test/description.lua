588c11eea2fdd54f3074f614ef82b220bebd11c4
Gospadin <gospadin@gmail.com>
2016-03-05 21:38:42 -0700

    Updated liveries for v377 unwrap, known issues on hump images/text

---
+++ A-4E/CoreMods/aircraft/A-4E/Liveries/A-4E/HoggitDev Test/description.lua
@@ -0,0 +1,13 @@
+-- rename it to description.lua
+livery = 
+{
+    {"fuselage", 0, "test.dds", false};
+}
+----== below part is not required for cockpit livery ==----
+--[[ name your own skin in default language (en)
+     meanwhile, you can also name the skin in more than one languages,
+     replace xx by [ru, cn, cs, de, es, fr, or it] ]]
+name = "HoggitDev Test"
+--name_xx = ""
+--[[ assign the countries ]]
+countries = {"USA"}
