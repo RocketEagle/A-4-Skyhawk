3d087fbdaed301b28b4209c498aa354f9520cc31
Gospadin <gospadin@gmail.com>
2016-03-08 23:14:40 -0700

    implement sequenced nose gear door (arg400)

--- A-4E/Mods/aircraft/A-4E/Cockpit/Scripts/Systems/gear.lua
+++ A-4E/Mods/aircraft/A-4E/Cockpit/Scripts/Systems/gear.lua
@@ -26,14 +26,20 @@ local Gear  = Keys.PlaneGear
 local GearUp = Keys.PlaneGearUp
 local GearDown = Keys.PlaneGearDown
 
-local GearNoseRetractTimeSec = 10 -- both gear take 10 seconds to retract
-local GearMainRetractTimeSec = 10 -- both gear take 10 seconds to retract
-local GearNoseExtendTimeSec = 10  -- nose gear sequencing is 10 seconds to extend fully
-local GearMainExtendTimeSec = 6   -- main gear sequencing is 6 seconds to extend fully
+local GearNoseRetractTimeSec = 8 -- both gear take 8 seconds to retract
+local GearMainRetractTimeSec = 8 -- both gear take 8 seconds to retract
+local GearNoseExtendTimeSec = 6  -- nose gear sequencing is 6 seconds to extend fully
+local GearMainExtendTimeSec = 5  -- main gear sequencing is 5 seconds to extend fully
+
+local GearNoseDoorTimeSec = 10   -- nose gear door takes 1 second to open/close (animation only goes 0.0 to 0.1)
+local GearMainDoorTimeSec = 1
 
 --Creating local variables
 local GEAR_NOSE_STATE = 0 -- 0 = retracted, 1.0 = extended -- "current" nose gear position
+local GEAR_NOSE_DOOR_STATE = 0
+
 local GEAR_MAIN_STATE =	0 -- 0 = retracted, 1.0 = extended -- "current" main gear position
+local GEAR_MAIN_DOOR_STATE = 0
 
 local GEAR_TARGET =     0 -- 0 = retracted, 1.0 = extended -- "future" gear position
 
@@ -82,12 +88,14 @@ function post_initialize()
   if wown == 1 or wowml == 1 or wowmr == 1 then
     GEAR_NOSE_STATE = 1
     GEAR_MAIN_STATE = 1
+    GEAR_NOSE_DOOR_STATE = 0.1
     GEAR_TARGET = 1
   end
   
   set_aircraft_draw_argument_value(0,GEAR_NOSE_STATE) -- nose gear draw angle
   set_aircraft_draw_argument_value(3,GEAR_MAIN_STATE) -- right gear draw angle
   set_aircraft_draw_argument_value(5,GEAR_MAIN_STATE) -- left gear draw angle
+  set_aircraft_draw_argument_value(400,GEAR_NOSE_DOOR_STATE) -- nose gear door draw angle
   
 end
 
@@ -98,6 +106,9 @@ function update()
 
     local gear_nose_extend_increment = update_time_step / GearNoseExtendTimeSec
     local gear_main_extend_increment = update_time_step / GearMainExtendTimeSec
+
+    local gear_nose_door_increment = update_time_step / GearNoseDoorTimeSec
+    local gear_main_door_increment = update_time_step / GearMainDoorTimeSec
 
     --[[if ONCE then
         -- if the simulation starts with velocity = 0, then assume we're on the ground and extend gear
@@ -139,12 +150,21 @@ function update()
         end
     end]]
     if GEAR_NOSE_ERR == 0 then
-      if GEAR_TARGET ~= GEAR_NOSE_STATE then
-        -- extending, use separate rates
+      if GEAR_TARGET ~= GEAR_NOSE_STATE or (GEAR_TARGET == 0 and GEAR_NOSE_DOOR_STATE > 0) then
         if GEAR_NOSE_STATE < GEAR_TARGET then
-          GEAR_NOSE_STATE = GEAR_NOSE_STATE + gear_nose_extend_increment
-        else
-          GEAR_NOSE_STATE = GEAR_NOSE_STATE - gear_nose_retract_increment
+            -- open door first, then extend the nosegear when door hits 0.1
+            if GEAR_NOSE_DOOR_STATE < 0.1 then
+                GEAR_NOSE_DOOR_STATE = GEAR_NOSE_DOOR_STATE + gear_nose_door_increment
+            else
+                GEAR_NOSE_STATE = GEAR_NOSE_STATE + gear_nose_extend_increment
+            end
+        elseif (GEAR_NOSE_STATE > GEAR_TARGET) or (GEAR_NOSE_DOOR_STATE > GEAR_TARGET) then
+            -- retract gear first, then when it hits 0, close the door
+            if GEAR_NOSE_STATE > 0 then
+                GEAR_NOSE_STATE = GEAR_NOSE_STATE - gear_nose_retract_increment
+            else
+                GEAR_NOSE_DOOR_STATE = GEAR_NOSE_DOOR_STATE - gear_nose_door_increment
+            end
         end
       end
     end
@@ -173,10 +193,17 @@ function update()
         GEAR_MAIN_STATE = 1
     end
 
+    if GEAR_NOSE_DOOR_STATE < 0 then
+        GEAR_NOSE_DOOR_STATE = 0
+    elseif GEAR_NOSE_DOOR_STATE > 0.1 then
+        GEAR_NOSE_DOOR_STATE = 0.1 -- 0.1 finishes the animation
+    end
+
     set_aircraft_draw_argument_value(0,GEAR_NOSE_STATE) -- nose gear draw angle
-	  set_aircraft_draw_argument_value(3,GEAR_MAIN_STATE) -- right gear draw angle
+    set_aircraft_draw_argument_value(3,GEAR_MAIN_STATE) -- right gear draw angle
     set_aircraft_draw_argument_value(5,GEAR_MAIN_STATE) -- left gear draw angle
-	
+
+    set_aircraft_draw_argument_value(400,GEAR_NOSE_DOOR_STATE) -- nose gear door
 end
 
 --need_to_be_closed = false -- close lua state after initialization
