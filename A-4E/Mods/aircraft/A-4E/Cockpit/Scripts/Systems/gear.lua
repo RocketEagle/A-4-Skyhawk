4cdb4e923dcddbd2d72191dc2ad4158380d8d6d4
Gospadin <gospadin@gmail.com>
2016-03-09 17:13:47 -0700

    Implement new gear code to match latest model, right main lags left by 0.7 seconds

--- A-4E/Mods/aircraft/A-4E/Cockpit/Scripts/Systems/gear.lua
+++ A-4E/Mods/aircraft/A-4E/Cockpit/Scripts/Systems/gear.lua
@@ -26,21 +26,17 @@ local Gear  = Keys.PlaneGear
 local GearUp = Keys.PlaneGearUp
 local GearDown = Keys.PlaneGearDown
 
-local GearNoseRetractTimeSec = 8 -- both gear take 8 seconds to retract
-local GearMainRetractTimeSec = 8 -- both gear take 8 seconds to retract
-local GearNoseExtendTimeSec = 6  -- nose gear sequencing is 6 seconds to extend fully
-local GearMainExtendTimeSec = 5  -- main gear sequencing is 5 seconds to extend fully
+local GearNoseRetractTimeSec = 8    -- 8 seconds to retract
+local GearNoseExtendTimeSec = 6     -- 6 seconds to extend
 
-local GearNoseDoorTimeSec = 10   -- nose gear door takes 1 second to open/close (animation only goes 0.0 to 0.1)
-local GearMainDoorTimeSec = 1
+local GearMainTimeSec = 5           -- 5 seconds to retract and extend main gear
+
+local LeftSideLead = 0.7 / (GearMainTimeSec) -- left side main gear leads right side, both opening and closing, by 0.7 seconds
 
 --Creating local variables
 local GEAR_NOSE_STATE = 0 -- 0 = retracted, 1.0 = extended -- "current" nose gear position
-local GEAR_NOSE_DOOR_STATE = 0
-
-local GEAR_MAIN_STATE =	0 -- 0 = retracted, 1.0 = extended -- "current" main gear position
-local GEAR_MAIN_DOOR_STATE = 0
-
+local GEAR_LEFT_STATE =	0 -- 0 = retracted, 1.0 = extended -- "current" main left gear position
+local GEAR_RIGHT_STATE = 0 -- 0 = retracted, 1.0 = extended -- "current" main right gear position
 local GEAR_TARGET =     0 -- 0 = retracted, 1.0 = extended -- "future" gear position
 
 local GEAR_NOSE_ERR   = 0
@@ -87,38 +83,23 @@ function post_initialize()
   local wowmr = sensor_data.getWOW_RightMainLandingGear()
   if wown == 1 or wowml == 1 or wowmr == 1 then
     GEAR_NOSE_STATE = 1
-    GEAR_MAIN_STATE = 1
-    GEAR_NOSE_DOOR_STATE = 0.1
+    GEAR_RIGHT_STATE = 1
+    GEAR_LEFT_STATE = 1
     GEAR_TARGET = 1
   end
   
   set_aircraft_draw_argument_value(0,GEAR_NOSE_STATE) -- nose gear draw angle
-  set_aircraft_draw_argument_value(3,GEAR_MAIN_STATE) -- right gear draw angle
-  set_aircraft_draw_argument_value(5,GEAR_MAIN_STATE) -- left gear draw angle
-  set_aircraft_draw_argument_value(400,GEAR_NOSE_DOOR_STATE) -- nose gear door draw angle
+  set_aircraft_draw_argument_value(3,GEAR_RIGHT_STATE) -- right gear draw angle
+  set_aircraft_draw_argument_value(5,GEAR_LEFT_STATE) -- left gear draw angle
   
 end
 
 
 function update()		
     local gear_nose_retract_increment = update_time_step / GearNoseRetractTimeSec
-    local gear_main_retract_increment = update_time_step / GearMainRetractTimeSec
-
     local gear_nose_extend_increment = update_time_step / GearNoseExtendTimeSec
-    local gear_main_extend_increment = update_time_step / GearMainExtendTimeSec
 
-    local gear_nose_door_increment = update_time_step / GearNoseDoorTimeSec
-    local gear_main_door_increment = update_time_step / GearMainDoorTimeSec
-
-    --[[if ONCE then
-        -- if the simulation starts with velocity = 0, then assume we're on the ground and extend gear
-        if sensor_data.getSelfVelocity() == 0 then
-            GEAR_NOSE_STATE = 1
-            GEAR_MAIN_STATE = 1
-            GEAR_TARGET = 1
-        end
-        ONCE = 0
-    end]]
+    local gear_main_increment = update_time_step / GearMainTimeSec
 
     ias_knots = sensor_data.getIndicatedAirSpeed() * 3.6 * rate_met2knot
     if ias_knots > 280 then 
@@ -130,54 +111,39 @@ function update()
       end
     end
   
-    -- make primary adjustment if needed
-    --[[if GEAR_TARGET == 1 then
-        -- extending, use separate rates
-        if GEAR_NOSE_STATE < GEAR_TARGET then
-            GEAR_NOSE_STATE = GEAR_NOSE_STATE + gear_nose_extend_increment
-    	end
-
-        if GEAR_MAIN_STATE < GEAR_TARGET then
-            GEAR_MAIN_STATE = GEAR_MAIN_STATE + gear_main_extend_increment
-        end
-    else
-        -- retracting, both gear retract at same speed
-        if GEAR_NOSE_STATE > GEAR_TARGET then
-            GEAR_NOSE_STATE = GEAR_NOSE_STATE - gear_nose_retract_increment
-        end
-        if GEAR_MAIN_STATE > GEAR_TARGET then
-            GEAR_MAIN_STATE = GEAR_MAIN_STATE - gear_main_retract_increment
-        end
-    end]]
+    -- make primary nosegear adjustments if needed
     if GEAR_NOSE_ERR == 0 then
-      if GEAR_TARGET ~= GEAR_NOSE_STATE or (GEAR_TARGET == 0 and GEAR_NOSE_DOOR_STATE > 0) then
-        if GEAR_NOSE_STATE < GEAR_TARGET then
-            -- open door first, then extend the nosegear when door hits 0.1
-            if GEAR_NOSE_DOOR_STATE < 0.1 then
-                GEAR_NOSE_DOOR_STATE = GEAR_NOSE_DOOR_STATE + gear_nose_door_increment
-            else
+        if GEAR_TARGET ~= GEAR_NOSE_STATE then
+            if GEAR_NOSE_STATE < GEAR_TARGET then
                 GEAR_NOSE_STATE = GEAR_NOSE_STATE + gear_nose_extend_increment
-            end
-        elseif (GEAR_NOSE_STATE > GEAR_TARGET) or (GEAR_NOSE_DOOR_STATE > GEAR_TARGET) then
-            -- retract gear first, then when it hits 0, close the door
-            if GEAR_NOSE_STATE > 0 then
-                GEAR_NOSE_STATE = GEAR_NOSE_STATE - gear_nose_retract_increment
             else
-                GEAR_NOSE_DOOR_STATE = GEAR_NOSE_DOOR_STATE - gear_nose_door_increment
+                GEAR_NOSE_STATE = GEAR_NOSE_STATE - gear_nose_retract_increment
             end
         end
-      end
     end
 
+    -- make primary main gear adjustments if needed
     if GEAR_MAIN_ERR == 0 then
-      if GEAR_TARGET ~= GEAR_MAIN_STATE then
-        -- extending, use separate rates
-        if GEAR_MAIN_STATE < GEAR_TARGET then
-          GEAR_MAIN_STATE = GEAR_MAIN_STATE + gear_main_extend_increment
-        else
-          GEAR_MAIN_STATE = GEAR_MAIN_STATE - gear_main_retract_increment
+        if GEAR_TARGET ~= GEAR_LEFT_STATE or GEAR_TARGET ~= GEAR_RIGHT_STATE then
+            -- left gear moves first, both up and down
+            if GEAR_LEFT_STATE < GEAR_TARGET then
+                -- extending
+                GEAR_LEFT_STATE = GEAR_LEFT_STATE + gear_main_increment
+            else
+                GEAR_LEFT_STATE = GEAR_LEFT_STATE - gear_main_increment
+            end
+
+            -- right gear lags left gear by LeftSideLead seconds
+            if GEAR_RIGHT_STATE < GEAR_TARGET then
+                if GEAR_LEFT_STATE > LeftSideLead then
+                    GEAR_RIGHT_STATE = GEAR_RIGHT_STATE + gear_main_increment
+                end
+            else
+                if GEAR_LEFT_STATE < (1-LeftSideLead) then
+                    GEAR_RIGHT_STATE = GEAR_RIGHT_STATE - gear_main_increment
+                end
+            end
         end
-      end
     end
             	
     -- handle rounding errors induced by non-modulo increment remainders
@@ -187,23 +153,22 @@ function update()
         GEAR_NOSE_STATE = 1
     end
 
-    if GEAR_MAIN_STATE < 0 then
-        GEAR_MAIN_STATE = 0
-    elseif GEAR_MAIN_STATE > 1 then
-        GEAR_MAIN_STATE = 1
+    if GEAR_LEFT_STATE < 0 then
+        GEAR_LEFT_STATE = 0
+    elseif GEAR_LEFT_STATE > 1 then
+        GEAR_LEFT_STATE = 1
     end
 
-    if GEAR_NOSE_DOOR_STATE < 0 then
-        GEAR_NOSE_DOOR_STATE = 0
-    elseif GEAR_NOSE_DOOR_STATE > 0.1 then
-        GEAR_NOSE_DOOR_STATE = 0.1 -- 0.1 finishes the animation
+    if GEAR_RIGHT_STATE < 0 then
+        GEAR_RIGHT_STATE = 0
+    elseif GEAR_RIGHT_STATE > 1 then
+        GEAR_RIGHT_STATE = 1
     end
 
     set_aircraft_draw_argument_value(0,GEAR_NOSE_STATE) -- nose gear draw angle
-    set_aircraft_draw_argument_value(3,GEAR_MAIN_STATE) -- right gear draw angle
-    set_aircraft_draw_argument_value(5,GEAR_MAIN_STATE) -- left gear draw angle
+    set_aircraft_draw_argument_value(3,GEAR_RIGHT_STATE) -- right gear draw angle
+    set_aircraft_draw_argument_value(5,GEAR_LEFT_STATE) -- left gear draw angle
 
-    set_aircraft_draw_argument_value(400,GEAR_NOSE_DOOR_STATE) -- nose gear door
 end
 
 --need_to_be_closed = false -- close lua state after initialization
