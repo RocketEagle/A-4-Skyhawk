c004097f44e04943c93b655f84d974cc341b8658
Alex Wiese <levelpulse@gmail.com>
2016-03-13 22:15:51 +0000

    Removed OLD A-4

--- Old/A-4E/comm.lua
+++
@@ -1,7 +0,0 @@
-local parameters = {
-	fighter = true, --true
-	radar = true,
-	ECM = false,
-	refueling = true --false
-}
-return utils.verifyChunk(utils.loadfileIn('Scripts/UI/RadioCommandDialogPanel/Config/LockOnAirplane.lua', getfenv()))(parameters)
