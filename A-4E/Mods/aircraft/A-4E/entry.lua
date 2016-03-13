340638a86f05bae2d4126de1b4041843a6d375a3
Alex Wiese <levelpulse@gmail.com>
2016-03-13 22:17:50 +0000

    Added info.

--- A-4E/Mods/aircraft/A-4E/entry.lua
+++ A-4E/Mods/aircraft/A-4E/entry.lua
@@ -8,7 +8,7 @@ fileMenuName = _("A-4E"),
 update_id        = "A-4E",
 version		 = "1.5.3 (WIP)",	-- 	 
 state		 = "installed",
-info		 = _("A-4E"),
+info		 = _("The A-4 is a light weight, single engined attack aircraft. The A-4, in all its variants, has participated in a wide range of wars, like the Vietnam war and all the way up to the Falklands war. The A4 aircraft is designed primarily for the delivery of coventional and nuclear weapons during hours of daylight and under visual flight conditions. However, these aircraft also have the capability of delivering conventional ordinance, under the control of the TPQ-10 radar, using either manual or automatic release modes at night and during foul weather conditions."),
 encyclopedia_path = current_mod_path..'/Encyclopedia',
 binaries   =
 {
